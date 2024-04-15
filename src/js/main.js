a20be0b43ad6c6e21a9ed0aec5e3d471c5c58f87
Hanin Farhan <haninfarhan96@gmail.com>
2024-04-15 14:07:35 +0200

    Skapat function som använder POST fetch API + DELETE för att ta bort cv + lagt till eventlistener på btn vid delete

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ window.onload = loadCv;
 
 async function loadCv() {
     document.getElementById("error").innerHTML = "";
+    document.getElementById("cv-wrap").innerHTML = "";
 
     try {
         const response = await fetch("https://dt207g-moment-2.onrender.com/api/cv");
@@ -27,5 +28,38 @@ function displayCv(cvData) {
                 <p class="cv-location">${cv.location}</p>
                 <p class="cv-description">${cv.description}</p>
             </article>`
+
+            let cvWrap = document.getElementById("cv-wrap");
+            let btnDelete = document.createElement("button");
+            let btnText = document.createTextNode("Ta bort");
+
+            btnDelete.appendChild(btnText);
+
+            cvWrap.appendChild(btnDelete);
+
+            btnDelete.addEventListener("click", () => {
+                deleteCv(cv.cv_id);
+            })
     });
+}
+
+async function deleteCv(cvId) {
+    //Fetch anrop för att lägga till cv med POST
+    try {
+        const response = await fetch("https://dt207g-moment-2.onrender.com/api/cv/" + cvId, {
+            method: "DELETE",
+        });
+
+        console.log(response);
+
+        if (!response.error) {
+            loadCv();
+
+        } else {
+            document.getElementById("error").innerHTML = "Det går inte att ta bort cv";
+        }
+
+    } catch {
+        document.getElementById("error").innerHTML = "Det gick inte att ta bort cv:" + error.message;
+    }
 }
